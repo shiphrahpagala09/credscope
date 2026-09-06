1ae8e77a147c24cf17d36784413de0ffcfa15456
Shiphrah Pagala <shiphrahpagala@gmail.com>
2026-09-06 14:01:42 +0530

    Initial commit: CredScope project skeleton

---
+++ src/main/java/com/credscope/credscope/CredscopeApplication.java
@@ -0,0 +1,13 @@
+package com.credscope.credscope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CredscopeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CredscopeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/credscope/credscope/CredscopeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.credscope.credscope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CredscopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
